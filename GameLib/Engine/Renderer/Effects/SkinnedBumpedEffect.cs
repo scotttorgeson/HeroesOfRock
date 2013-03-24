d5b51e4e2a2fdca8af64e6f03b7c93b34577bead
Scott Torgeson <scott.torgeson@gmail.com>
2013-03-23 22:45:26 -0600

    SkinnedBumpedEffect inherits from SkinnedEffect, instead of BumpedEffect.

--- GameLib/Engine/Renderer/Effects/SkinnedBumpedEffect.cs
+++ GameLib/Engine/Renderer/Effects/SkinnedBumpedEffect.cs
@@ -7,68 +7,29 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GameLib
 {
-    public class SkinnedBumpedEffect : BumpedEffect
+    public class SkinnedBumpedEffect : SkinnedEffect
     {
-        public const int MaxBones = 75;
-
         #region Effect Parameters
 
-        EffectParameter bonesParam;
+        EffectParameter normalMapTextureParam;
 
         #endregion
 
-        /// <summary>
-        /// Sets an array of skinning bone transform matrices.
-        /// </summary>
-        public void SetBoneTransforms(Matrix[] boneTransforms)
+        public Texture2D NormalMapTexture
         {
-            if ((boneTransforms == null) || (boneTransforms.Length == 0))
-                throw new ArgumentNullException("boneTransforms");
-
-            if (boneTransforms.Length > MaxBones)
-                throw new ArgumentException();
-
-            bonesParam.SetValue(boneTransforms);
-        }
-
-
-        /// <summary>
-        /// Gets a copy of the current skinning bone transform matrices.
-        /// </summary>
-        public Matrix[] GetBoneTransforms(int count)
-        {
-            if (count <= 0 || count > MaxBones)
-                throw new ArgumentOutOfRangeException("count");
-
-            Matrix[] bones = bonesParam.GetValueMatrixArray(count);
-
-            // Convert matrices from 43 to 44 format.
-            for (int i = 0; i < bones.Length; i++)
-            {
-                bones[i].M44 = 1;
-            }
-
-            return bones;
+            get { return normalMapTextureParam.GetValueTexture2D(); }
+            set { normalMapTextureParam.SetValue(value); }
         }
 
         public SkinnedBumpedEffect(Effect effect)
             : base(effect)
         {
             CacheEffectParameters();
-
-            Matrix[] identityBones = new Matrix[MaxBones];
-
-            for (int i = 0; i < MaxBones; i++)
-            {
-                identityBones[i] = Matrix.Identity;
-            }
-
-            SetBoneTransforms(identityBones);
         }
 
         void CacheEffectParameters()
         {
-            bonesParam = Parameters["Bones"];
+            normalMapTextureParam = Parameters["NormalMap"];
         }
     }
 }
